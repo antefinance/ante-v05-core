52764790d4b18dee0be155a9cdde14f38f0b75ad
Jon Shih <js18059@sf-js18059-mac1.localdomain>
2021-08-18 11:04:42 -0700

    updated comments for readability

--- contracts/examples/AnteAVLDropTest.sol
+++ contracts/examples/AnteAVLDropTest.sol
@@ -14,36 +14,36 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../AnteTest.sol";
 
-// Ante Test to check if AVL in Ante created Tests drops by 99%
+// Ante Test to check if AVL in Ante-created tests drops by 99%
 contract AnteAVLDropTest is AnteTest("Ante Doesnt Lose 99% of its AVL") {
     using SafeMath for uint256;
 
     uint256 public totalAVL;
     uint256 public aVLThreshold;
     
-    // contracts deployed with
+    // Array of contract addresses to test should be passed in when creating
     constructor (address[] memory _testedContracts) {
         protocolName = "Ante Finance";
         testedContracts = _testedContracts;
 
-        //loop over address and sum up total
+        // Loop over address array and sum up total AVL
         for (uint i = 0; i < testedContracts.length; i++) {
             totalAVL = totalAVL.add(testedContracts[i].balance);
         }
         
-        //calculate threshold using 99% drop in total
+        // Calculate test failure threshold using 99% drop in total AVL
         aVLThreshold = totalAVL.div(100);
     }
     
     function checkTestPasses() public view override returns (bool) {
         uint256 currentAVL;
 
-        //sum the total
+        // Sum up current AVL across tested contracts
         for (uint i = 0; i < testedContracts.length; i++) {
             currentAVL = currentAVL.add(testedContracts[i].balance);
         }
 
-        //check against threshold
+        // Check current AVL against test failure threshold
         return currentAVL >= aVLThreshold;
     }
 }
